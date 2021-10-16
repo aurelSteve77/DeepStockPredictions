d1854f70b4a94e03772115644229b6b55ee974ca
Aurel Steve Avomo <aurel-steve.avomo@grenoble-inp.org>
2021-10-16 08:28:45 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predict the stock market using stocks data and news articles by using deep learning techniques',
+    author='aurelSteve77',
+    license='MIT',
+)
